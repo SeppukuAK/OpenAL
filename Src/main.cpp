8502bddf39b4927378b6a2250a12dc1604f6dd85
Raúl Serrano Gómez <raulserranodev@gmail.com>
2018-10-24 10:58:48 +0200

    Comentado codigo

--- Src/main.cpp
+++ Src/main.cpp
@@ -3,24 +3,46 @@
 #include <alut.h>
 #include <alc.h>
 
-int main() {
-	// inicializamos
-	alutInit(0, NULL);
-	alGetError(); //limpiamos el bit de error
-				  // creamos buffer
-	//ALuint Buffer = alutCreateBufferFromFile("../wavdata/holaMundo.wav");
-	ALuint Buffer = alutCreateBufferFromFile("../../Media/batalla/Battle.wav");
+/*
+3 objetos básicos:
+Buffer. Almacena el sonido. Puede ser utilizado por diferentes sources.
+Sources. Punto del espacio que emite snidos. Se pueden modificar propiedades. (Posicion y velocidad)
+Listener. Posición, orientación y velocidad del oyente. Se pueden modificar propiedades.
 
+Unidades-
+
+Sources.
+alSourcefv(). Una única función para modificar propiedades -
+
+Contexto. Estado de OpenAL (escena). Se pueden tener varios e ir intercambiando.-
+
+El código está con Alut. Elige la mejor configuracion. S epuede ir a mas bajo nivel con AL
+
+*/
+
+int main() {
+	//Inicialización OpenAL
+	alutInit(0, NULL);	//Alut 
+	alGetError(); //limpiamos el bit de error
+
+	//Creación buffer
+	ALuint Buffer = alutCreateBufferFromFile("../../Media/batalla/Battle.wav");
 	if (alGetError() != AL_NO_ERROR) exit(-1);
-	// creamos source
+
+	//Creación source
 	ALuint Source;
 	alGenSources(1, &Source);
-	// enganchamos source al buffer
+
+	//Enganchar buffer a source
 	alSourcei(Source, AL_BUFFER, Buffer);
-	// reproducimos
+
+	//Reproduccion
 	alSourcePlay(Source);
-	// pausa para escuchar, liberacion de recursos y salida
+
+	//Pausa para escuchar
 	alutSleep(3);
+
+	//Liberacíon de recursos y salida
 	alutExit();
 	return EXIT_SUCCESS;
 }
